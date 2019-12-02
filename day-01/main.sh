1de20aa45474aa8522c5a60ae164189fc3437dc4
Eric Lemieux <eric@lemieuxdev.com>
2019-12-02 10:33:05 -0500

    Completing day 1 - part 2

--- day-01/main.sh
+++ day-01/main.sh
@@ -1,18 +1,33 @@
 #!/usr/bin/env bash
 
 function find_fuel() {
-  weight=${1}
-  div=$((weight/3))
-  final=$((div-2))
+  local weight=${1}
+  local div=$((weight/3))
+  local fuel=$((div-2))
+  local fuel_for_fuel=0
+
+  if [[ $fuel -gt 0 ]];
+  then
+    fuel_for_fuel=$(find_fuel $fuel)
+  fi
+
+  local final=$((fuel+fuel_for_fuel))
+
+  if [[ $final -lt 0 ]];
+  then
+    final=0
+  fi
+
   echo "${final}"
 }
 
 function main() {
-  sum=0
+  local sum=0
 
   # Read the input file and sum the result of all the elements
   while read -r line
   do
+    local fuel=0
     fuel=$(find_fuel line)
     sum=$((sum+fuel))
   done < "${1:-/dev/stdin}"
